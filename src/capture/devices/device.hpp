2c3cac0501e14bdfc36bccefd73791e1b2ab6ee3
J. Santos <jamillo@gmail.com>
2016-07-08 14:56:55 -0300

    fix #1 Implements the first abstraction of a `Device`

---
+++ src/capture/devices/device.hpp
@@ -0,0 +1,37 @@
+/**
+ * @author J. Santos <jamillo@gmail.com>
+ * @date July 08, 2016
+ */
+
+#ifndef MOTE_VISION_CAPTURE_DEVICE_HPP
+#define MOTE_VISION_CAPTURE_DEVICE_HPP
+
+#include <opencv/ml.h>
+
+namespace mote
+{
+namespace capture
+{
+class Device
+{
+public:
+	/**
+	 * Reads a frame from the device.
+	 */
+	virtual bool read(cv::Mat &image) = 0;
+
+	/**
+	 * Return if the device is open.
+	 */
+	virtual bool isOpen() = 0;
+
+	/**
+	 * Closes the device and release the device.
+	 */
+	virtual void close() = 0;
+};
+}
+}
+
+
+#endif //MOTE_VISION_CAPTURE_DEVICE_HPP
