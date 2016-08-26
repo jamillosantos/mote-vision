fb84c310d5123f32086207904134613f576915a7
J. Santos <jamillo@gmail.com>
2016-08-26 10:52:19 -0300

    Implementation of constructors for `data::Pixel` and its tests

--- src/data/pixel.h
+++ src/data/pixel.h
@@ -17,6 +17,10 @@ struct Pixel
 	uint8_t b;
 	uint8_t g;
 	uint8_t r;
+
+	Pixel();
+
+	Pixel(uint8_t r, uint8_t g, uint8_t b);
 };
 }
 }
---
+++ test/src/data/pixel_test.cpp
@@ -0,0 +1,24 @@
+/**
+ * @author J. Santos <jamillo@gmail.com>
+ * @date July 20, 2016
+ */
+
+#include <gtest/gtest.h>
+#include <data/pixel.h>
+
+GTEST_TEST(data_pixel, constructor)
+{
+	mote::data::Pixel p(1, 2, 3);
+	ASSERT_EQ(1, p.r);
+	ASSERT_EQ(2, p.g);
+	ASSERT_EQ(3, p.b);
+}
+
+GTEST_TEST(data_pixel, address_alignment)
+{
+	char* data = "\x03\x02\x01";
+	mote::data::Pixel *p = (mote::data::Pixel*)data;
+	ASSERT_EQ(1, p->r);
+	ASSERT_EQ(2, p->g);
+	ASSERT_EQ(3, p->b);
+}
